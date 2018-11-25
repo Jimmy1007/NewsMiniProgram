1960ddf7052bb74ceb05b1a2f53a5459e093eddf
Jimmy1007 <623492978@qq.com>
2018-11-25 10:06:39 +0800

    分类选项框架

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,14 @@
+const categories = [
+  { code: 'gn', name: '国内' },
+  { code: 'gj', name: '国际' },
+  { code: 'cj', name: '财经' },
+  { code: 'yl', name: '娱乐' },
+  { code: 'js', name: '军事' },
+  { code: 'ty', name: '体育' },
+  { code: 'other', name: '其他' }
+];
+
+
 Page({
 
   
